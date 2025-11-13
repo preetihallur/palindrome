6efc9ac27eda30bf7900ed5e4ec23b2d25176d96
preetihallur <preetibhallur@gmail.com>
2025-11-13 09:54:58 +0530

    Correct palindrome check logic
    Fix logic to check for palindrome in user input.

--- palindrome.py
+++ palindrome.py
@@ -1,14 +1,12 @@
 # palindrome_check.py
 # Program to check if a given string is a palindrome
 import sys
-def is_palindrome(s):
     # Remove spaces and convert to lowercase
     s = s.replace(" ", "").lower()
     return s == s[::-1]
-
-if __name__ == "__main__":
+    
     user_input = sys.argv[1]
-    if is_palindrome(user_input):
+    if s==user_input:
         print(f"'{user_input}' is a palindrome.")
     else:
         print(f"'{user_input}' is not a palindrome.")
