183445e023aa935efe51e9b13091427509f76771
preetihallur <preetibhallur@gmail.com>
2025-11-13 09:58:01 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,11 +1,8 @@
 # palindrome_check.py
 # Program to check if a given string is a palindrome
 import sys
-    # Remove spaces and convert to lowercase
-    s = s.replace(" ", "").lower()
-    return s == s[::-1]
-    
     user_input = sys.argv[1]
+    s == s[::-1]
     if s==user_input:
         print(f"'{user_input}' is a palindrome.")
     else:
