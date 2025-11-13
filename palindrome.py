7c149ba3303f35ce3b6c58deeb52ce44ed7843f9
preetihallur <preetibhallur@gmail.com>
2025-11-13 09:51:57 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,13 +1,13 @@
 # palindrome_check.py
 # Program to check if a given string is a palindrome
-
+import sys
 def is_palindrome(s):
     # Remove spaces and convert to lowercase
     s = s.replace(" ", "").lower()
     return s == s[::-1]
 
 if __name__ == "__main__":
-    user_input = input("Enter a string: ")
+    user_input = sys.argv[1]
     if is_palindrome(user_input):
         print(f"'{user_input}' is a palindrome.")
     else:
